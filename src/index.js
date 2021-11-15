b2ba057b0c6b4c1314106d91a1331b5886984d88
Katie Pundt <kzpundt@gmail.com>
2021-11-15 10:40:36 -0800

    add additional forecast info
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

--- src/index.js
+++ src/index.js
@@ -24,8 +24,22 @@ $(document).ready(function() {
 
    function getElements(response) {
       const fahrenheit = Math.round((((response.main.temp-273.15)*1.8)+32));
+      const fahrenheitFeels = Math.round((((response.main.feels_like-273.15)*1.8)+32));
+      const fahrenheitMin = Math.round((((response.main.temp_min-273.15)*1.8)+32));
+      const fahrenheitMax = Math.round((((response.main.temp_max-273.15)*1.8)+32));
+      const sunrise = new Date(response.sys.sunrise * 1000);
+      const sunset= new Date(response.sys.sunset * 1000);
+      // const sunrise = Math.round((((response.main.temp_max-273.15)*1.8)+32));
+      // const sunset = Math.round((((response.main.temp_max-273.15)*1.8)+32));
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${fahrenheit} degrees.`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${fahrenheit} degrees(F).`);
+      $('.showTempFeels').text(`It currently feels like ${fahrenheitFeels} degrees(F).`);
+      $('.showTempMin').text(`Minimum temperature for today ${fahrenheitMin} degrees(F)`);
+      $('.showTempMax').text(`Maximum temperature for today ${fahrenheitMax} degrees(F)`);
+      $('.windSpeed').text(`Current wind speed is ${response.wind.speed} mph`);
+      $('.sunrise').text(`Sunrise at ${sunrise}`);
+      $('.sunset').text(`Sunset at ${sunset}`);
+
     }
   });
 });
