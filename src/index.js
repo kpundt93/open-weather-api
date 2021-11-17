f012df0ba952c75cf7cb627951b5904caadafdda
Katie Pundt <kzpundt@gmail.com>
2021-11-16 18:29:04 -0800

    refactor to use async functions

--- src/index.js
+++ src/index.js
@@ -20,34 +20,35 @@ function clearFields() {
 
 function getElements(response) {
   if (response.main) {
-    $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+    const fahrenheit = Math.round((((response.main.temp-273.15)*1.8)+32));
+    const fahrenheitFeels = Math.round((((response.main.feels_like-273.15)*1.8)+32));
+    const fahrenheitMin = Math.round((((response.main.temp_min-273.15)*1.8)+32));
+    const fahrenheitMax = Math.round((((response.main.temp_max-273.15)*1.8)+32));
+    const sunrise = new Date(response.sys.sunrise * 1000);
+    const sunset= new Date(response.sys.sunset * 1000);
+    $('.showHumidity').text(`The humidity is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Fahrenheit is ${fahrenheit} degrees(F).`);
     $('.showTempFeels').text(`It currently feels like ${fahrenheitFeels} degrees(F).`);
     $('.showTempMin').text(`Minimum temperature for today ${fahrenheitMin} degrees(F)`);
     $('.showTempMax').text(`Maximum temperature for today ${fahrenheitMax} degrees(F)`);
-    $('.windSpeed').text(`Current wind speed is ${body.wind.speed} mph`);
+    $('.windSpeed').text(`Current wind speed is ${response.wind.speed} mph`);
     $('.sunrise').text(`Sunrise at ${sunrise}`);
     $('.sunset').text(`Sunset at ${sunset}`);
   } else {
-    $('.showErrors').text(`There was an error processing your request: ${error}`);
+    $('.showErrors').text(`There was an error processing your request: ${response}`);
   }
+}
+
+async function makeApiCall(city, zip) {
+  const response = await WeatherService.getWeather(city, zip);
+  getElements(response);
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     let zip = $('#zip').val();
-    const body = JSON.parse(response);
-    const fahrenheit = Math.round((((body.main.temp-273.15)*1.8)+32));
-    const fahrenheitFeels = Math.round((((body.main.feels_like-273.15)*1.8)+32));
-    const fahrenheitMin = Math.round((((body.main.temp_min-273.15)*1.8)+32));
-    const fahrenheitMax = Math.round((((body.main.temp_max-273.15)*1.8)+32));
-    const sunrise = new Date(body.sys.sunrise * 1000);
-    const sunset= new Date(body.sys.sunset * 1000);
     clearFields();
-    WeatherService.getWeather(city, zip)
-      .then(function(response) {
-      getElements(response);
-    });    
+    makeApiCall(city, zip); 
   });
 });
